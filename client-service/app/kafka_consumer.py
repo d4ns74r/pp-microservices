62f817b759a17dc1757d0c358b0696cf72a25eab
D4NS74R <97897939+d4ns74r@users.noreply.github.com>
2024-12-16 23:04:49 +0300

    add logger to clients, kafka consumer, payments

--- client-service/app/kafka_consumer.py
+++ client-service/app/kafka_consumer.py
@@ -2,9 +2,8 @@ import asyncio
 import json
 from aiokafka import AIOKafkaConsumer
 from confluent_kafka.admin import AdminClient, NewTopic
-import logging
+from app.logger import logger
 
-logging.basicConfig(level=logging.INFO)
 
 class KafkaConsumerService:
     def __init__(self, kafka_broker: str, topic: str):
@@ -15,31 +14,32 @@ class KafkaConsumerService:
 
     async def create_topic_if_not_exists(self):
         try:
-            logging.info(f"Checking if topic '{self.topic}' exists...")
+            logger.info(f"Checking if topic '{self.topic}' exists...")
             admin_client = AdminClient({'bootstrap.servers': self.kafka_broker})
             metadata = admin_client.list_topics(timeout=10)
 
             if self.topic not in metadata.topics:
-                logging.info(f"Topic '{self.topic}' not found. Creating topic...")
+                logger.info(f"Topic '{self.topic}' not found. Creating topic...")
                 new_topic = NewTopic(self.topic, num_partitions=1, replication_factor=1)
                 fs = admin_client.create_topics([new_topic])
                 for topic, f in fs.items():
                     try:
                         f.result()  # Блокирует до завершения
-                        logging.info(f"Topic '{self.topic}' created successfully.")
+                        logger.info(f"Topic '{self.topic}' created successfully.")
                     except Exception as e:
-                        logging.error(f"Failed to create topic {topic}: {e}")
+                        logger.info(f"Failed to create topic {topic}: {e}")
+
             else:
-                logging.info(f"Topic '{self.topic}' already exists.")
+                logger.info(f"Topic '{self.topic}' already exists.")
         except Exception as e:
-            logging.error(f"Error creating or checking topic: {e}")
+            logger.error(f"Error creating or checking topic: {e}")
 
     async def start(self):
         try:
-            logging.info("Waiting for Kafka to be ready...")
+            logger.info("Waiting for Kafka to be ready...")
             await self.wait_for_kafka()
 
-            logging.info(f"Starting Kafka consumer for topic '{self.topic}'...")
+            logger.info(f"Starting Kafka consumer for topic '{self.topic}'...")
 
             # Создание топика, если его нет
             await self.create_topic_if_not_exists()
@@ -53,32 +53,32 @@ class KafkaConsumerService:
                 group_id="my_consumer_group"
             )
 
-            logging.info(f"Connecting to Kafka...")
+            logger.info(f"Connecting to Kafka...")
             await self.consumer.start()
             self.task = asyncio.create_task(self.consume_messages())
-            logging.info("Kafka consumer started")
+            logger.info("Kafka consumer started")
         except Exception as e:
-            logging.error(f"Error starting Kafka consumer: {e}")
+            logger.error(f"Error starting Kafka consumer: {e}")
 
     async def stop(self):
         if self.consumer:
-            logging.info("Stopping Kafka consumer...")
+            logger.info("Stopping Kafka consumer...")
             await self.consumer.stop()
             if self.task:
                 self.task.cancel()
                 try:
                     await self.task
                 except asyncio.CancelledError:
-                    logging.info("Consumer task cancelled")
-            logging.info("Kafka consumer stopped")
+                    logger.info("Consumer task cancelled")
+            logger.info("Kafka consumer stopped")
 
     async def consume_messages(self):
         while True:
             try:
                 async for message in self.consumer:
-                    logging.info(f"Received message: {message.value}")
+                    logger.info(f"Received message: {message.value}")
             except Exception as e:
-                logging.error(f"Error in Kafka consumer loop: {e}")
+                logger.error(f"Error in Kafka consumer loop: {e}")
                 await asyncio.sleep(5)  # Задержка перед повторной попыткой
 
     async def wait_for_kafka(self):
@@ -86,17 +86,18 @@ class KafkaConsumerService:
         for _ in range(retries):
             try:
                 # Пробуем получить метаданные от Kafka
-                admin_client = AdminClient({'bootstrap.servers': self.kafka_broker})
-                metadata = admin_client.list_topics(timeout=5)
-                logging.info("Kafka is ready!")
+                AdminClient({'bootstrap.servers': self.kafka_broker})
+                logger.info("Kafka is ready!")
                 return
             except Exception as e:
-                logging.warning(f"Kafka is not ready yet, retrying: {e}")
+                logger.warning(f"Kafka is not ready yet, retrying: {e}")
                 await asyncio.sleep(5)  # Ждем 5 секунд перед следующей попыткой
         raise Exception("Kafka is not available after multiple retries")
 
+
 # Инициализация глобального Kafka Consumer
 kafka_consumer = KafkaConsumerService(kafka_broker="kafka:9092", topic="payment_notifications")
+
 
 # Пример старта
 async def run():
